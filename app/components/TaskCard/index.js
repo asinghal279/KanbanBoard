b6c3a74c5186192d913e1c8d32f5410e0f4257ab
Raghav Singhal <raghav.singhal@mountblue.tech>
2020-11-18 22:35:42 +0530

    Alter the truncated functionality for the content of the card

--- app/components/TaskCard/index.js
+++ app/components/TaskCard/index.js
@@ -8,6 +8,7 @@ import React, { useState } from 'react';
 import { Draggable } from 'react-beautiful-dnd';
 import {
   Box,
+  ButtonGroup,
   Editable,
   EditableInput,
   EditablePreview,
@@ -15,8 +16,50 @@ import {
   IconButton,
   Input,
   Text,
+  Textarea,
 } from '@chakra-ui/core';
-import { DeleteIcon } from '@chakra-ui/icons';
+import {
+  CheckIcon,
+  CloseIcon,
+  DeleteIcon,
+  DragHandleIcon,
+  EditIcon,
+  InfoIcon,
+} from '@chakra-ui/icons';
+
+function CustomControlsExample() {
+  /* Here's a custom control */
+  function EditableControls({ isEditing, onSubmit, onCancel, onEdit }) {
+    return isEditing ? (
+      <ButtonGroup justifyContent="center" size="sm">
+        <IconButton icon={<CheckIcon />} onClick={onSubmit} />
+        <IconButton icon={<CloseIcon />} onClick={onCancel} />
+      </ButtonGroup>
+    ) : (
+      <Flex justifyContent="center">
+        <IconButton size="sm" icon={<EditIcon />} onClick={onEdit} />
+      </Flex>
+    );
+  }
+
+  return (
+    <Editable
+      textAlign="center"
+      defaultValue="Rasengan ⚡️"
+      fontSize="2xl"
+      isPreviewFocusable={false}
+      submitOnBlur={false}
+    >
+      {props => (
+        <>
+          <EditablePreview />
+          <EditableInput />
+          <EditableControls {...props} />
+        </>
+      )}
+    </Editable>
+  );
+}
 
 function Task({ id, index, content, columnId, deleteItem, editItem }) {
   const [contentValue, setContentValue] = useState(content);
@@ -27,7 +70,6 @@ function Task({ id, index, content, columnId, deleteItem, editItem }) {
         return (
           <Flex
             draggable
-            h={20}
             p={4}
             justify="space-between"
             bg="white"
@@ -38,23 +80,48 @@ function Task({ id, index, content, columnId, deleteItem, editItem }) {
             {...provided.dragHandleProps}
             style={{ ...provided.draggableProps.style }}
           >
-            <Input
+            {/* <CustomControlsExample /> */}
+            <Textarea
               value={contentValue}
               onChange={e => setContentValue(e.target.value)}
               onBlur={() => editItem(index, columnId, contentValue)}
               padding={0}
               outline="none"
+              isFullWidth
+              size="fit-content"
               border="none"
-              w="fit-content"
               fontWeight={600}
+              style={{ overflow: 'hidden' }}
             />
-            <IconButton
-              size="sm"
-              bg="none"
-              color="black"
-              icon={<DeleteIcon />}
-              onClick={() => deleteItem(index, columnId)}
-            />
+            <Flex direction="column">
+              <IconButton
+                size="sm"
+                bg="none"
+                color="black"
+                icon={<DeleteIcon />}
+                onClick={() => deleteItem(index, columnId)}
+              />
+              <IconButton
+                size="sm"
+                bg="none"
+                color="black"
+                icon={<InfoIcon />}
+                bg="none"
+                _hover={{
+                  bg: 'none',
+                }}
+              />
+              <IconButton
+                size="sm"
+                bg="none"
+                color="black"
+                icon={<DragHandleIcon />}
+                bg="none"
+                _hover={{
+                  bg: 'none',
+                }}
+              />
+            </Flex>
           </Flex>
         );
       }}
